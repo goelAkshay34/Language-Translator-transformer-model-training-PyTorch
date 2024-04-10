5cd50cd2dacb49d21f5c5548cdd92841a9ded575
Akshay Goel <114993729+goelAkshay34@users.noreply.github.com>
2024-04-11 04:50:48 +0530

    Update config.py

--- config.py
+++ config.py
@@ -18,7 +18,8 @@ def get_config():
     }
 
 def get_weights_file_path(config, epoch: str):
-    model_folder = f"{config['datasource']}_{config['model_folder']}"
+    #model_folder = f"{config['datasource']}_{config['model_folder']}"
+    model_folder = f"{config['model_folder']}"
     model_filename = f"{config['model_basename']}{epoch}.pt"
     return str(Path('.') / model_folder / model_filename)
 
